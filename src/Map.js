be6daf91308d4538c8a1f7905bf4ee4a030817d9
haydar yıldırım <haydarkayra@gmail.com>
2023-02-26 19:14:00 +0300

    sadece 1 marker konacak şekilde güncellendi

--- src/Map.js
+++ src/Map.js
@@ -6,6 +6,7 @@ import './map.css';
 export default function Map(){
   const mapContainer = useRef(null);
   const map = useRef(null);
+  const marker = useRef(null);
   const [lng] = useState(139.753);
   const [lat] = useState(35.6844);
   const [zoom] = useState(8);
@@ -25,13 +26,15 @@ export default function Map(){
         const longitude = lngLat.lng;
         const latitude = lngLat.lat;
         
+        // Önceki markörü kaldır
+        if (marker.current) {
+          marker.current.remove();
+        }
+
         // Markörü oluştur
-        new maplibregl.Marker({color: "#FF0000"})
-        .setLngLat([longitude, latitude])
-        .addTo(map.current);
-        
-       
-       
+        marker.current = new maplibregl.Marker({color: "#FF0000"})
+          .setLngLat([longitude, latitude])
+          .addTo(map.current);
       });
     }
   }, [lng, lat, zoom, API_KEY]);
