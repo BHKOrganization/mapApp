fe6e077999dcfb98a0e6dee3a2bebc92af38f9a6
haydaryldrm <haydarkayra@gmail.com>
2023-02-27 12:48:38 +0300

    main kısmı eklendi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import Map from './components/Map/Map';
+import Main from './components/main/Main';
 function App() {
   return (
     <div className="App">
-    <Map/>
+    <Main/>
     </div>
   );
 }
---
+++ src/components/playBtn/PlayBtn.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './playBtn.css';
+
+export const PlayBtn = () => {
+  return (
+    <div>
+        <button>PLAY</button>
+    </div>
+  )
+}
