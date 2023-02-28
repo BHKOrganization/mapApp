26646b4971b0e0995356beefd5a89b67a871f219
haydaryldrm <haydarkayra@gmail.com>
2023-02-28 14:13:59 +0300

    Add a FAQPage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
 import { GamePage } from "./pages/GamePage";
 import { HomePage } from "./pages/HomePage";
-
+import { FAQPage } from "./pages/FAQPage";
 function App() {
-  
   return (
     <div className="App">
       <div>
-        <HomePage/>
+        <FAQPage />
       </div>
     </div>
   );
